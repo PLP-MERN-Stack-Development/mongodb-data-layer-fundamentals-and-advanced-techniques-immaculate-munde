395e1633a3f04fd73d8b5ec315c6a6b40dda829f
Immaculate Munde <immaculatemunde1@gmail.com>
2025-10-21 19:06:32 +0300

    Add script to insert sample book data into MongoDB collection

--- insert_books.js
+++ insert_books.js
@@ -4,7 +4,7 @@
 const { MongoClient } = require('mongodb');
 
 // Connection URI (replace with your MongoDB connection string if using Atlas)
-const uri = 'mongodb://localhost:27017';
+const uri = 'mongodb+srv://munde:munde@cluster0.deyeof3.mongodb.net/';
 
 // Database and collection names
 const dbName = 'plp_bookstore';
